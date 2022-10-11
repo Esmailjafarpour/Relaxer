2241ab9625bde07723a243c6da6eb9f7df3b19d2
nader <esmailjafarpour29@gmail.com>
2022-10-11 16:58:08 +0330

    Relaxer project code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState,useEffect} from 'react';
+ 
 
 function App() {
+
+    const [breathing, setBreathing] = useState('Breathe In');
+    const [scale, setScale] = useState('container-grow');
+    const [color, setColor] = useState('');
+
+    const totalTime = 7500;
+    const breateTime = (totalTime/5)*2;
+    const holdTime = totalTime/5;
+
+    const updateBreathe = () => {
+      
+      setBreathing("Breathe In")
+      setColor("circle-BreatheIn")
+        setTimeout(() => {
+
+          setBreathing("hold")
+          setColor("circle-hold")
+
+          setTimeout(() =>{ 
+
+            setColor("")
+            setBreathing("Breathe out")
+            setScale("container-shrink")
+
+            setTimeout(() => {
+
+              setScale("container-grow")
+              updateBreathe()
+              
+            }, breateTime);
+
+          }, holdTime)
+
+        }, breateTime);
+        
+      }
+  
+      useEffect(() => {
+
+        updateBreathe()
+  
+      },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Relaxer</h1>
+      <div className={`container ${scale}`} >
+          <div className={`circle ${color}`}></div>
+          <p className="text">{breathing}</p>
+          <div className="pointer-container">
+            <span className="pointer"></span>
+          </div>
+          <div className="gradient-circle"></div>
+      </div>
     </div>
-  );
+  )
+
 }
 
 export default App;
